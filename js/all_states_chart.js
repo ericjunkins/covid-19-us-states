b40e972e03f489c30f0c442e372f7db357d5a73a
ericjunkins <eric.junkins@colorado.edu>
2020-04-16 11:12:50 -0700

    reorganizaing files

--- js/all_states_chart.js
+++ js/all_states_chart.js
@@ -1,25 +1,9 @@
 function states_chart(config){
-    var margin = { left:100, right:100, top:50, bottom:80 }
-    
-    let data_by_states;
+    var margin = { left:120, right:10, top:50, bottom:80 }
     let data_list;
-    let date_range;
-    let states;
-    let colors = {};
-    var display_states;
-    let text_locations = [];
-    let test_annotations = [];
-    let line_labels = [];
-    let sah_dots = [];
-    var sah_data = (config.annotations == undefined ? {} : config.annotations)
-    var abbrev2full = (config.abbrev2full == undefined ? {} : config.abbrev2full)
-    var full2abbrev = (config.full2abbrev == undefined ? {} : config.full2abbrev)
-    var states_time_data =(config.state_data == undefined ? {} : config.state_data);
-    var parseDate_sah = config.parseDate
-    var parseTime = d3.timeParse("%Y%m%d");
-    var formatTime = d3.timeFormat("%m/%d/%y");
-    
-    
+    var anno = config.anno;
+    var abbrev2full = config.abbrev2full;
+
     var height = config.height - margin.top - margin.bottom, 
         width = config.width - margin.left - margin.right;
     
@@ -31,12 +15,11 @@ function states_chart(config){
             .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
-    var text_g = svg.append("g")
     
     var x = d3.scaleLog()
         .domain([1, 1000000])
         .range([0,width])
-
+          
     var y = d3.scaleLog()
         .domain([1, 100000])
         .range([height, 0])
@@ -55,10 +38,10 @@ function states_chart(config){
         .attr("class", "axis axis--y axisWhite")
         .call(y_axis)
     
-        svg.append("text")
+    svg.append("text")
         .attr("class", "axis-text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
+        .attr("y", 0 - 90)
         .attr("x", 0 - (height/2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
@@ -75,8 +58,6 @@ function states_chart(config){
         .attr("font-size", "2rem")
         .text("Total Confirmed Cases")
     
-    
-    
     dots = svg.append('g')
     circ = svg.append('g')
     line = d3.line()
@@ -92,9 +73,6 @@ function states_chart(config){
         })
         //.curve(d3.curveBasis)
         .curve(d3.curveCardinal.tension(0.5))
-
-
-    var table = document.getElementById("selection-table")
     
     var makeAnnotations = d3.annotation()
         .type(d3.annotationLabel)
@@ -114,207 +92,24 @@ function states_chart(config){
         .call(line_annotations)
     
 
-    organize_data();
-    init_display();
-
     function statesChart(){
-        //update_display_data();
         draw_chart();
     }
 
-    function init_display(){
-        init_checkboxes()
-        init_slider()
-        display_states = ['NY', 'CA', 'CO', 'AK', 'MT']
-        display_states.forEach(function(d,i){
-            d3.select("#chbox_" + d).property('checked', true)
-            colors[d] = color(i)
-        })
-    }
-    
-    function init_checkboxes(){
-        states = d3.keys(data_by_states);
-        for (var i=0; i< states.length; i +=2){
-            var checkbox1 = document.createElement('input');
-            var checkbox2 = document.createElement('input');
-            var table_row = table.insertRow(-1)
-            var cell1 = table_row.insertCell(0);
-            var cell2 = table_row.insertCell(1);
-            checkbox1.setAttribute("class", "c")
-            checkbox2.setAttribute("class", "c")
-            checkbox1.type = "checkbox";
-            checkbox2.type = "checkbox";
-            checkbox1.name = states[i];
-            checkbox2.name = states[i+1];
-            checkbox1.checked = false;
-            checkbox2.checked = false;
-            checkbox1.value = 0;
-            checkbox2.value = 0;
-            checkbox1.id = "chbox_" + states[i]
-            checkbox2.id = "chbox_" + states[i+1]
-    
-            cell1.appendChild(checkbox1);
-            cell2.appendChild(checkbox2);
-            // // creating label for checkbox 
-            var label1 = document.createElement('label'); 
-            var label2 = document.createElement('label');  
-            // // assigning attributes for  
-            // // the created label tag  
-            label1.htmlFor = "id"; 
-            label2.htmlFor = "id"; 
-    
-            label1.appendChild(document.createTextNode(abbrev2full[states[i]])); 
-            label2.appendChild(document.createTextNode(abbrev2full[states[i+1]])); 
-            cell1.appendChild(label1)
-            cell2.appendChild(label2)
-            
-            checkbox1.addEventListener('change', (event) => {
-                var action = (event.target.checked ? 'add' : 'remove')
-                update_display_states(event.target.name, action)
-            })
-            checkbox2.addEventListener('change', (event) => {
-                var action = (event.target.checked ? 'add' : 'remove')
-                update_display_states(event.target.name, action)
-    
-            })
-        }
-    }
-    
-    function organize_data(){
-        data_by_states = d3.nest()
-                .key(function(d){ return d.state})
-                .object(states_time_data)
-    
-        date_range = d3.extent(states_time_data, function(d){
-            return d.date;
-        })
-    
-        const entries = Object.entries(data_by_states);
-        for (const [key, vals] of entries){
-            var window = []
-            
-            for (var i = vals.length-1; i >= 0; i--){
-                window.push(vals[i].positiveIncrease)
-                if (window.length >4){
-                    window.shift()
-                }
-                var total = (window.length == 1 ? window[0] : d3.sum(window) )
-                data_by_states[key][i]['binnedPositiveIncrease'] = total
-                if (sah_data[abbrev2full[key]].date != null){
-                    if (vals[i].date.getTime() == sah_data[abbrev2full[key]].date.getTime()){
-                        sah_data[abbrev2full[key]]['binnedPositiveIncrease'] = vals[i].binnedPositiveIncrease
-                        sah_data[abbrev2full[key]]['positive'] = vals[i].positive
-                        sah_data[abbrev2full[key]]['annotation'] = {
-                            'note': {
-                                'label': abbrev2full[key],
-                                'title': 'Stay at home order issued: ' + formatTime(vals[i].date)
-                            },      
-                            'connector': {
-                                'end': "dot",
-                                'endScale': 8
-                            },
-                            'disable': [],
-                            'x': x(vals[i].positive),
-                            'y': y(vals[i].binnedPositiveIncrease),
-                            'dy': -75,
-                            'dx': -75
-                        }
-                    }
-                }
-            }
-            if (sah_data[abbrev2full[key]].date == null){
-                sah_data[abbrev2full[key]].date = parseDate_sah("3/19/40")
-                sah_data[abbrev2full[key]]['binnedPositiveIncrease'] = 'banana'
-                sah_data[abbrev2full[key]]['positive'] = 0
-                sah_data[abbrev2full[key]]['annotation'] = {
-                    'note': {
-                        'label': abbrev2full[key],
-                        'title': 'None' 
-                    },
-                    'subject': {
-                        'radius': 15,
-                        'stroke': 10
-                    },
-                    'connector': {
-                        'end': "dot",
-                        'endScale': 1
-                    },
-                    'disable': ['subject, note, connector'],
-                    'x': -1000,
-                    'y': -1000,
-                    'dy': -50,
-                    'dx': -50
-                }
-            }
-    
-            sah_data[abbrev2full[key]]['line_label'] = {
-                'note': {
-                    'label': abbrev2full[key],
-                },
-                'connector': {
-                    'end': "dot",
-                    'endScale': 2
-                },
-    
-                'disable': ["subject", "connector"],
-                'x': x(1),
-                'y': y(1),
-                'dy': 0,
-                'dx': 60
-            }
-        }
-    }
-    
-    function update_display_states(state, action){
-        colors = {}
-        if (action == "add"){
-            display_states.push(state)
-        } else {
-            const index = display_states.indexOf(state)
-            display_states.splice(index, 1)
-        }
-        test_annotations = []
-        display_states.forEach(function(d, i){
-            test_annotations.push(sah_data[abbrev2full[d]].annotation)
-            colors[d] = color(i)
-        })
-        //update_display_data()
-    }
-    
-    // function update_display_data(){
-    //     data_list = []
-    //     sah_dots = []
-    //     display_states.forEach(function(d,i){
-    //         var res = data_by_states[d].filter(function(v){
-    //             return v.date < sliderTime.value();
-    //         })
-    //         if (res.length != 0){
-    //             data_list.push(res)
-    //         }
-    //         if (sah_data[abbrev2full[d]].date <= sliderTime.value()){
-    //             var x = sah_data[abbrev2full[d]].positive
-    //             var y = sah_data[abbrev2full[d]].binnedPositiveIncrease
-    //             sah_dots.push({'x': x, 'y': y, 'state': d})
-    //         }
-    //     })
-    //     draw_chart(data_list)
-    // }
-    
     function draw_chart(data){
         draw_paths()
-        // draw_leading_dots();
-        // draw_sah_dots();
-        // draw_line_labels();
+        draw_stay_home_dots();
+        draw_leading_dots();
+        draw_annotations(line_labels, 'line');
     }
     
     function draw_paths(){
-        console.log('data_list', data_list);
         lines = svg.selectAll(".line")
             .data(data_list, function(d){ return d[0].state })
-            .on("mousemove", moved)
-            .on("mouseenter", entered)
-            .on("mouseleave", leave)
-            .on("click", entered)
+                .on("mousemove", moved)
+                .on("mouseenter", entered)
+                .on("mouseleave", leave)
+                .on("click", entered)
     
         lines.exit().remove()
     
@@ -342,29 +137,9 @@ function states_chart(config){
     
     }
     
-    function draw_line_labels(){
-        text_locations = []
-        line_labels = []
-        data_list.forEach(function(d, i){
-            var tmp = sah_data[abbrev2full[d[0].state]]['line_label']
-            tmp['x'] = x((d[0].positive == 0 ? 1 : d[0].positive))
-            tmp['y'] = y(d[0].binnedPositiveIncrease == 0 ? 1 : d[0].binnedPositiveIncrease)
-            tmp['color'] = color(i)
-    
-            text_locations.push(d[0])
-            line_labels.push(tmp)
-    
-        })
-    
-        line_annotations
-            .type(d3.annotationLabel)
-            .annotations(line_labels)
-    
-    }
-    
-    function draw_sah_dots(){
+    function draw_stay_home_dots(){
         dot = dots.selectAll("circle")
-            .data(sah_dots, function(d){ return d.state})
+            .data(stayHomeDots, function(d){ return d.state})
     
         dot.exit().remove()
     
@@ -377,6 +152,7 @@ function states_chart(config){
     
         dot.enter()
             .append("circle")
+            .attr("class", "stayHomeDot")
             .attr("opacity", 1)
             .style('fill', function(d,i){ return colors[d.state]; })
             .attr('cx', function(d){ return x((d.x == 0 ? 1 : d.x))})
@@ -385,7 +161,7 @@ function states_chart(config){
             .transition().duration(750)
                 .attr("r", 8.5)
     }
-    
+
     function draw_leading_dots(){
         circles = circ.selectAll("circle")
             .data(text_locations, function(d){ return d.state})
@@ -405,12 +181,58 @@ function states_chart(config){
             .attr("cy", function(d){ return y((d.binnedPositiveIncrease == 0 ? 1 : d.binnedPositiveIncrease))})
             .attr("r", 3)
     }
+
+    function update_display_data(){
+        data_list = []
+        stayHomeDots = []
+        text_locations = []
+        line_labels = []
+
+        display_states.forEach(function(d, i){
+            var res = data_by_states[d].filter(function(v){
+                return v.date < curTime;
+            })
+            if (res.length != 0){
+                data_list.push(res)
+            }
+
+            if (anno[abbrev2full[d]].date <= curTime && anno[abbrev2full[d]].date != null ){
+                stayHomeDots.push({
+                    'x': anno[abbrev2full[d]].positive,
+                    'y': anno[abbrev2full[d]].binnedPositiveIncrease,
+                    'state': d
+                })
+            }
+            
+        })
+        data_list.forEach(function(d, i){
+            var tmp = anno[abbrev2full[d[0].state]]['line_label']
+            tmp['x'] = x((d[0].positive == 0 ? 1 : d[0].positive))
+            tmp['y'] = y(d[0].binnedPositiveIncrease == 0 ? 1 : d[0].binnedPositiveIncrease)
+            tmp['color'] = color(i)
+
+            text_locations.push(d[0])
+            line_labels.push(tmp)
+        })
+    }
     
+    function draw_annotations(anno, type){
+        if (type == 'line'){
+            line_annotations
+                .type(d3.annotationLabel)
+                .annotations(anno)
+        } else  if (type == 'dot'){
+            makeAnnotations
+                .type(d3.annotationCallout)
+                .annotations(anno)
+        }
+    }
+
     function moved (d){
         //console.log('moved', d3.event)
     }
     
-    function entered(d,i){
+    function entered (d,i){
         svg.selectAll(".line")
             .attr("stroke", "#e0e0e0")
             .attr("opacity", function(v,j){
@@ -439,35 +261,31 @@ function states_chart(config){
             .attr("stroke-width", 6.5)
             .attr("stroke", color(i))
     
-        if (sah_data[abbrev2full[d[0].state]].date <= sliderTime.value()){
-            tmp = sah_data[abbrev2full[d[0].state]].annotation
+        if (anno[abbrev2full[d[0].state]].date <= curTime){
+            tmp = anno[abbrev2full[d[0].state]].annotation
             tmp.color = color(i)
-            makeAnnotations
-                .type(d3.annotationCallout)
-                .annotations([tmp])
+            draw_annotations([tmp], 'dot')
         }
     
-        var tmp = sah_data[abbrev2full[d[0].state]]['line_label']
-        tmp['x'] = x((d[0].positive == 0 ? 1 : d[0].positive))
-        tmp['y'] = y(d[0].binnedPositiveIncrease == 0 ? 1 : d[0].binnedPositiveIncrease)
-        tmp['color'] = color(i)
+        var tmp = anno[abbrev2full[d[0].state]]['line_label']
+            tmp['x'] = x((d[0].positive == 0 ? 1 : d[0].positive))
+            tmp['y'] = y(d[0].binnedPositiveIncrease == 0 ? 1 : d[0].binnedPositiveIncrease)
+            tmp['color'] = color(i)
     
-        line_annotations
-            .type(d3.annotationLabel)
-            .annotations([tmp])
+        draw_annotations([tmp], 'line')
+
     
     }   
     
-    function leave(){
+    function leave (){
         draw_leading_dots();
         draw_paths();
-        draw_sah_dots();
-        draw_line_labels();
-        makeAnnotations
-            .type(d3.annotationLabel)
-            .annotations([])
+        draw_stay_home_dots();
+        draw_annotations(line_labels, 'line')
+        draw_annotations([], 'dot')
     
     }
+    
 
     statesChart.width = function(value){
         if (!arguments.length) return width;
@@ -487,15 +305,53 @@ function states_chart(config){
         return statesChart;
     }
 
-    statesChart.annotations = function(value){
-        if(!arguments.length) return annotations;
-        annotations = value;
+    statesChart.anno = function(value){
+        if(!arguments.length) return anno;
+        anno = value;
         return stateChart
     }
 
     statesChart.data_list = function(value){
         if(!arguments.length) return data_list;
         data_list = value;
+        return statesChart;
+    }
+
+    statesChart.stayHomeDots = function(value){
+        if(!arguments.length) return stayHomeDots;
+        stayHomeDots = value;
+        return statesChart;       
+    }
+
+    statesChart.colors = function(value){
+        if(!arguments.length) return colors;
+        colors = value;
+        return statesChart;       
+    }
+
+    statesChart.curTime = function(value){
+        if(!arguments.length) return curTime;
+        curTime = value;
+        update_display_data();
+        return statesChart;   
+    }
+
+    statesChart.x = function(value){
+        if(!arguments.length) return x;
+        x = value;
+        return statesChart;
+    }
+
+    statesChart.y = function(value){
+        if(!arguments.length) return y;
+        y = value;
+        return statesChart;
+    }
+
+    statesChart.display_states = function(value){
+        if(!arguments.length) return this.display_states;
+        display_states = value;
+        update_display_data();
         return statesChart;
     }
 
--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
-
 let full2abbrev = {};
 let abbrev2full = {};
 let states_vis;
 let states_time_data;
+let data_by_states;
+let date_range;
+let annotations;
+var display_states = [];
+var colors = [];
+var text_locations = [];
+var line_labels = [];
 var parseTime = d3.timeParse("%Y%m%d");
 var formatTime = d3.timeFormat("%m/%d/%y");
-var display_states = ['NY', 'CA', 'CO', 'AK', 'MT'];
 
 var promises = [
     d3.json("data/abbreviations.json"),
     d3.json("data/stay_at_home.json")
 ]
 
+var table = document.getElementById("selection-table")
+
 Promise.all(promises).then(ready)
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 var sliderTime = d3.sliderBottom()
     .step(1000 * 60 * 60 * 24)
     .height(100)
     .fill('#616161')
-    .width(1500)
+    .width(1100)
     .step(86400000)
     .on('onchange', val=> {
         d3.select('p#value-time').text(formatTime(val));
@@ -28,27 +37,27 @@ var sliderTime = d3.sliderBottom()
 
 var gTime = d3.select('div#slider-time')
     .append('svg')
-        .attr('width', 1800)
+        .attr('width', 1500)
         .attr('height', 150)
         .append('g')
             .attr("class", "slider-tick")
             .attr('transform', 'translate(250,75)')
+        
 
-
-function ready([abbrev2full, anno]){
+function ready([abbrev, anno]){
     var full2abbrev = {}
-    const abb_entries = Object.entries(abbrev2full);
+    const abb_entries = Object.entries(abbrev);
         for (const [key, vals] of abb_entries){
             full2abbrev[vals] = key
     }
 
+    abbrev2full = abbrev;
     var parseDate = d3.timeParse("%m/%d/%y")
     const anno_entries = Object.entries(anno);
     for (const [key, vals] of anno_entries){
-        //console.log(key, entries)
         anno[key].date = parseDate(vals.date)
     }
-
+    annotations = anno
     var request = new XMLHttpRequest()
     request.open("GET", "https://covidtracking.com/api/states/daily", true)
     request.onload = function(){ 
@@ -67,25 +76,127 @@ function ready([abbrev2full, anno]){
             if(d.death == null){d.death = 0}
             if(d.pending == null){d.pending = 0}
         })
-
-
         var allStatesConfig = {
             'height':600,
-            'width': 1800,
-            'abbrev2full': abbrev2full,
-            'annotations': anno,
+            'width': 1500,
             'parseDate': parseDate,
             'state_data': states_time_data,
+            'anno': anno,
+            'abbrev2full': abbrev2full,
             'duration': 250,
             'selection': '#slip-chart'
         }
-
-        states_vis = states_chart(allStatesConfig)
-        //states_vis();
+        states_vis = states_chart(allStatesConfig);
+        init_display();
+        selection_init();
+        update_vis();
     }
     request.send()
 }
 
+function init_display(){
+    display_states = ['NY', 'CA', 'CO', 'AK', 'MT']
+    organize_data();
+    //init_checkboxes();
+    init_slider();
+    display_states.forEach(function(d,i){
+        //d3.select("#chbox_" + d).property('checked', true)
+        colors[d] = color(i)
+    })
+}
+
+function update_vis(){
+    states_vis.curTime(sliderTime.value())
+    states_vis.display_states(display_states);
+    states_vis();
+}
+
+function organize_data(){
+    data_by_states = d3.nest()
+        .key(function(d){ return d.state; })
+        .object(states_time_data)
+
+    date_range = d3.extent(states_time_data, function(d){
+        return d.date;
+    })
+
+    var x = states_vis.x(),
+        y = states_vis.y()
+    const entries = Object.entries(data_by_states);
+    for (const [key, vals] of entries){
+        var window = []
+        
+        for (var i = vals.length-1; i >= 0; i--){
+            window.push(vals[i].positiveIncrease)
+            if (window.length >4){
+                window.shift()
+            }
+            var total = (window.length == 1 ? window[0] : d3.sum(window) )
+            data_by_states[key][i]['binnedPositiveIncrease'] = total
+            if (annotations[abbrev2full[key]].date != null){
+                if (vals[i].date.getTime() == annotations[abbrev2full[key]].date.getTime()){
+                    annotations[abbrev2full[key]]['binnedPositiveIncrease'] = vals[i].binnedPositiveIncrease
+                    annotations[abbrev2full[key]]['positive'] = vals[i].positive
+                    annotations[abbrev2full[key]]['annotation'] = {
+                        'note': {
+                            'label': abbrev2full[key],
+                            'title': 'Stay at home order issued: ' + formatTime(vals[i].date)
+                        },      
+                        'connector': {
+                            'end': "dot",
+                            'endScale': 8
+                        },
+                        'disable': [],
+                        'x': x(vals[i].positive),
+                        'y': y(vals[i].binnedPositiveIncrease),
+                        'dy': -75,
+                        'dx': -75
+                    }
+                }
+            }
+        }
+        if (annotations[abbrev2full[key]].date == null){
+            annotations[abbrev2full[key]].date = parseTime("3/19/40")
+            annotations[abbrev2full[key]]['binnedPositiveIncrease'] = 'banana'
+            annotations[abbrev2full[key]]['positive'] = 0
+            annotations[abbrev2full[key]]['annotation'] = {
+                'note': {
+                    'label': abbrev2full[key],
+                    'title': 'None' 
+                },
+                'subject': {
+                    'radius': 15,
+                    'stroke': 10
+                },
+                'connector': {
+                    'end': "dot",
+                    'endScale': 1
+                },
+                'disable': ['subject, note, connector'],
+                'x': -1000,
+                'y': -1000,
+                'dy': -50,
+                'dx': -50
+            }
+        }
+
+        annotations[abbrev2full[key]]['line_label'] = {
+            'note': {
+                'label': abbrev2full[key],
+            },
+            'connector': {
+                'end': "dot",
+                'endScale': 2
+            },
+
+            'disable': ["subject", "connector"],
+            'x': x(1),
+            'y': y(1),
+            'dy': 0,
+            'dx': 60
+        }
+    }
+}
 
 function init_slider(){
     date_range = d3.extent(states_time_data, function(d){
@@ -102,24 +213,79 @@ function init_slider(){
     d3.select('p#value-time').text(formatTime(sliderTime.value()));
 }
 
-function update_vis(){
-    var data_list = []
-    var sah_dots = []
-    display_states.forEach(function(d,i){
-        var res = data_by_states[d].filter(function(v){
-            return v.date < sliderTime.value();
+function init_checkboxes(){
+    states = d3.keys(data_by_states);
+    for (var i=0; i< states.length; i +=2){
+        var checkbox1 = document.createElement('input');
+        var checkbox2 = document.createElement('input');
+        var table_row = table.insertRow(-1)
+        var cell1 = table_row.insertCell(0);
+        var cell2 = table_row.insertCell(1);
+        checkbox1.setAttribute("class", "c")
+        checkbox2.setAttribute("class", "c")
+        checkbox1.type = "checkbox";
+        checkbox2.type = "checkbox";
+        checkbox1.name = states[i];
+        checkbox2.name = states[i+1];
+        checkbox1.checked = false;
+        checkbox2.checked = false;
+        checkbox1.value = 0;
+        checkbox2.value = 0;
+        checkbox1.id = "chbox_" + states[i]
+        checkbox2.id = "chbox_" + states[i+1]
+
+        cell1.appendChild(checkbox1);
+        cell2.appendChild(checkbox2);
+        // // creating label for checkbox 
+        var label1 = document.createElement('label'); 
+        var label2 = document.createElement('label');  
+        // // assigning attributes for  
+        // // the created label tag  
+        label1.htmlFor = "id"; 
+        label2.htmlFor = "id"; 
+
+        label1.appendChild(document.createTextNode(abbrev2full[states[i]])); 
+        label2.appendChild(document.createTextNode(abbrev2full[states[i+1]])); 
+        cell1.appendChild(label1)
+        cell2.appendChild(label2)
+        
+        checkbox1.addEventListener('change', (event) => {
+            var action = (event.target.checked ? 'add' : 'remove')
+            update_display_states(event.target.name, action)
         })
-        if (res.length != 0){
-            data_list.push(res)
-        }
-        // if (sah_data[abbrev2full[d]].date <= sliderTime.value()){
-        //     var x = sah_data[abbrev2full[d]].positive
-        //     var y = sah_data[abbrev2full[d]].binnedPositiveIncrease
-        //     sah_dots.push({'x': x, 'y': y, 'state': d})
-        // }
-    })
-    states_vis.data_list(data_list)
-    states_vis();
+        checkbox2.addEventListener('change', (event) => {
+            var action = (event.target.checked ? 'add' : 'remove')
+            update_display_states(event.target.name, action)
+
+        })
+
+        checkbox1.addEventListener("mouseenter", console.log("mouseenter"))
+    }
 }
 
+function update_display_states(state, action){
+    colors = {}
+    if (action == "add"){
+        display_states.push(state)
+    } else {
+        const index = display_states.indexOf(state)
+        display_states.splice(index, 1)
+    }
+    test_annotations = []
+    display_states.forEach(function(d, i){
+        test_annotations.push(annotations[abbrev2full[d]].annotation)
+        colors[d] = color(i)
+    })
+    update_vis();
+}
 
+function selection_init(){
+    var selectionConfig = {
+        'height':700,
+        'width': 300,
+        'selection': '#state-selection'
+    }
+    var selection_vis = stateSelector(selectionConfig);
+    selection_vis.states(d3.keys(data_by_states))
+    selection_vis();
+}
